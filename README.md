ac52abb7c85945e789be23761757b1a401b5f7ff
alextahan <atahan@ucsd.edu>
2023-05-28 22:50:50 -0700

    Completed Expose and Explore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+
+Alexander Tahan
+
+1. I would fit in my automated tests by manually running them locally before pushing code. This way, I can run tests whenever I want to, and don't risk missing a bug until pushing the code, when it may be difficult to go back and fix it. When done correctly, this also ensures that all the code that is pushed has passed all tests.
+2. No, I would not use E2E Testing to check if a function is returning the correct output.
+3. No. The message feature of a messaging application has many factors that would be difficult to unit test on a large scale. Changing other features of the app would likely affect the test and it would be hard to narrow down the cause of the test failing.
+4. Yes. Max message length is a small function that would be easy and useful to implement a unit test for. A unit test would make it easy to identify if the function is implemented incorrectly.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,89 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//isPhoneNumber()
+test('checks 858-123-4567', () => {
+    expect(functions.isPhoneNumber('858-123-4567')).toBeTruthy();
+});
+
+test('checks (858)123-4567', () => {
+    expect(functions.isPhoneNumber('(858)123-4567')).toBeTruthy();
+});
+
+test('checks (858) 123 4567', () => {
+    expect(functions.isPhoneNumber('(858) 123 4567')).toBeFalsy();
+});
+
+test('checks 123-456-789', () => {
+    expect(functions.isPhoneNumber('123-456')).toBeFalsy();
+});
+
+//isEmail()
+test('checks jdoe@ucsd.edu', () => {
+    expect(functions.isEmail('jdoe@ucsd.edu')).toBeTruthy();
+});
+
+test('checks jdoeucsd.edu', () => {
+    expect(functions.isEmail('jdoeucsd.edu')).toBeFalsy();
+});
+
+test('checks jdoe@ucsd', () => {
+    expect(functions.isEmail('jdoe@ucsd')).toBeFalsy();
+});
+
+test('checks jdoe@gmail.com', () => {
+    expect(functions.isEmail('jdoe@gmail.com')).toBeTruthy();
+});
+
+
+//isStrongPassword()
+test('checks 12345678', () => {
+    expect(functions.isStrongPassword('12345678')).toBeFalsy();
+});
+
+test('checks a12345678', () => {
+    expect(functions.isStrongPassword('a12345678')).toBeTruthy();
+});
+
+test('checks a1234567812345678', () => {
+    expect(functions.isStrongPassword('a1234567812345678')).toBeFalsy();
+});
+
+test('checks suPer_Str0ng_pW', () => {
+    expect(functions.isStrongPassword('suPer_Str0ng_pW')).toBeTruthy();
+});
+
+//isDate()
+test('checks 5/28/2023', () => {
+    expect(functions.isDate('5/28/2023')).toBeTruthy();
+});
+
+test('checks 05/28/2023', () => {
+    expect(functions.isDate('05/28/2023')).toBeTruthy();
+});
+
+test('checks 5/28/23', () => {
+    expect(functions.isDate('5/28/23')).toBeFalsy();
+});
+
+test('checks 5-28-2023', () => {
+    expect(functions.isDate('5-28-2023')).toBeFalsy();
+});
+
+//isHexColor()
+test('check #FFF', () => {
+    expect(functions.isHexColor('#FFF')).toBeTruthy();
+});
+
+test('check 1a2b3f', () => {
+    expect(functions.isHexColor('1a2b3f')).toBeTruthy();
+});
+
+test('check #GGG', () => {
+    expect(functions.isHexColor('#GGG')).toBeFalsy();
+});
+
+test('check 12FF', () => {
+    expect(functions.isHexColor('12FF')).toBeFalsy();
+});
+
